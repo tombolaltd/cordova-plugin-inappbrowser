ab243e7fc42cfe647754439de8096965d63376fd
David Sanderson <david.sanderson@tombola.com>
2020-08-24 13:13:26 +0100

    re-add closing to re-add orientation

--- src/ios/CDVWKInAppBrowser.m
+++ src/ios/CDVWKInAppBrowser.m
@@ -405,8 +405,10 @@ static CDVWKInAppBrowser* instance = nil;
     dispatch_async(dispatch_get_main_queue(), ^{
         if (self.inAppBrowserViewController != nil) {
             self->_previousStatusBarStyle = -1;
+            // Adding closing of the IAB inside of hiding to fix orientation delegate overriding cordova webview
             NSURL* blankURL = [NSURL URLWithString: @"about:blank"];
             [self.inAppBrowserViewController navigateTo: blankURL];
+            [self close:nil];
         }
     });
 }
